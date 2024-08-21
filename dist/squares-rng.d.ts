98d949816370dbd56b68bddf1f35ac79c69b028f
duncan <dwerner@riskamp.com>
2024-08-21 12:47:57 -0700

    update declarations

--- dist/squares-rng.d.ts
+++ dist/squares-rng.d.ts
@@ -8,13 +8,28 @@ export declare class SquaresRNG {
     protected fills: number;
     /** don't call the constructor. use the factory method. */
     protected constructor(key: bigint, start: bigint);
+    /**
+     * reset to change the key or the counter. in addition to
+     * updating the values, it will flush the cache so you don't
+     * get stale values.
+     *
+     * @param key
+     * @param start
+     */
     Reset(key: bigint, start?: bigint | number): void;
+    /** returns a random double in [0, 1] */
     Next(): number;
+    /** fills an array with random doubles */
     NextN(n: number, array?: Float64Array): Float64Array;
     /**
-     * create an instance, using a seed to generate the key. optionally
-     * advance the counter.
+     * create an instance. sets key and counter (default 0).
+     *
+     * this method is async because WASM is async, and we need to ensure
+     * that WASM is initialized before you make any calls to the sample
+     * functions.
+     *
      */
     static CreateInstance(key: bigint, start_counter?: number | bigint): Promise<SquaresRNG>;
+    /** generate n samples */
     protected Fill(n: number, array: Float64Array): void;
 }
